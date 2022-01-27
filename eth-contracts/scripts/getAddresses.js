75743868736289ba0329c41c9327e862a87012b5
Reuben Rodrigues <reubenrodrigues24@gmail.com>
2022-01-27 19:32:25 +0530

    Latest deployment

--- eth-contracts/scripts/getAddresses.js
+++ eth-contracts/scripts/getAddresses.js
@@ -1,4 +1,4 @@
-const {promisify} = require("es6-promisify");
+// const {promisify} = require("es6-promisify");
 const addresses = require('../migrations/migration-output.json')
 const tokenAddress = addresses.tokenAddress
 const registryAddress = addresses.registryAddress
